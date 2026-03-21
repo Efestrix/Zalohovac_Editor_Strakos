042309c269ef4a16fccdbee72758040280640573
Jakub Strakoš <StrakosJakub@sssvt.cz>
2026-03-21 17:09:12 +0100

    added Buttons and Helper

---
+++ Zalohovac_Editor_Strakos/Presentation/Components/Button.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zalohovac_Editor_Strakos.Presentation.Windows;
+
+namespace Zalohovac_Editor_Strakos.Presentation.Components
+{
+    public class Button : BaseComponent
+    {
+        public event Action? Clicked;
+
+        public override bool Selectable => true;
+
+        private string _text;
+
+        public Button(string text, bool inline = false)
+            : base(inline)
+        {
+            _text = text;
+        }
+
+        public override void Render(bool selected)
+        {
+            if (selected)
+                Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write($"[ {_text} ]");
+            Console.ResetColor();
+
+            base.Render(selected);
+        }
+
+        public override void HandleKey(ConsoleKeyInfo keyInfo)
+        {
+            if (keyInfo.Key == ConsoleKey.Enter)
+                Clicked?.Invoke();
+        }
+    }
+}
---
+++ Zalohovac_Editor_Strakos/Presentation/Windows/BackupJobWindow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zalohovac_Editor_Strakos.Entities;
+using Zalohovac_Editor_Strakos.Presentation.Components;
+
+namespace Zalohovac_Editor_Strakos.Presentation.Windows
+{
+    public class BackupJobWindow : BaseWindow
+    {
+        private BackupJob _backupJob;
+
+        private TextBox _method;
+        private TextBox _timing;
+        private TextBox _retention;
+        private TextBox _source;
+        private TextBox _targets;
+
+        public BackupJobWindow(string title, Application application, IWindow? returnWindow, List<IComponent> components, int selectedIndex) 
+            : base("Přehled záloh", application, returnWindow, components, selectedIndex)
+        {
+            _method = new TextBox("Metoda: \n", 32);
+            _timing = new TextBox("Časování: \n", 16);
+            _retention = new TextBox("Retence: \n", 64);
+            _source = new TextBox("Zdroj: \n", 64);
+            _targets = new TextBox("Cíle: \n", 64);
+
+        }
+    }
+}
